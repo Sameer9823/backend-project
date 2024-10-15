dacef4de1442cf30b2c1b38180fbe941575587e5
Sameer9823 <sameerselokar924@gmail.com>
2024-10-15 16:55:36 +0530

    bug fixed

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -26,17 +26,25 @@ const registerUser = asyncHandler(async(req, res) => {
         
     }
 
-   const existedUser= User.findOne({
+   const existedUser= await User.findOne({
         $or: [{username}, {email}]
     })
 
     if(existedUser){
         throw new ApiError(409, "User already exists");
     }
+    // console.log(req.files);
+    
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
-    
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0 ){
+        coverImageLocalPath = req.files.coverImage[0].path;
+        
+    }
+
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar is required");
     }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: 'auto',
         })
         //file has been uploaded successfull
-        console.log("file uploaded on cloudinary", response.url);
+        // console.log("file uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath);
         return response
         
         
