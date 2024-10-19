e2a5627bb3ca84416c344b722fb0093c8788170a
Sameer9823 <sameerselokar924@gmail.com>
2024-10-19 16:07:01 +0530

    add comment like playlist tweet models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,7 +9,8 @@ import {
     updatedAvatar, 
     updatedCoverImage, 
     getUserChannelProfile, 
-    getWatchHistory
+    getWatchHistory,
+    getCurrentUser
 } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
@@ -36,7 +37,7 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
-router.route("/current-user").get(verify, getCurrentUser)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-detail").patch(verifyJWT, updateAccountDetail)
 router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updatedAvatar)
 router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updatedCoverImage)
